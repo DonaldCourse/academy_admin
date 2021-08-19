3b0e66dbc25ddcc3e6b939b34b0df9d7245cda5b
minhtri <trilqdola@gmail.com>
2021-08-19 22:34:59 +0700

    build docker for web admin

--- src/contants/index.js
+++ src/contants/index.js
@@ -1,6 +1,2 @@
-export const FILE_URL =
-    process.env.REACT_APP_BASE_URL_CDN ||
-    "https://cdn.tingtong.xyz/files";
-export const BASE_URL = process.env.REACT_APP_BASE_URL + ":" + process.env.REACT_APP_PORT;
-export const SOCKET_URL =
-    process.env.REACT_APP_BASE_URL_SOCKET || "socket.tingtong.xyz";
+export const FILE_URL = process.env.REACT_APP_BASE_URL_CDN || "http://103.130.218.153:5001";
+export const BASE_URL = process.env.REACT_APP_BASE_URL + ":" + process.env.REACT_APP_PORT || "http://103.130.218.153:5000";
